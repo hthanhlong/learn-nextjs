0f58b8b24cce8cf3c784fee51d0d39206cfee0b7
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-09-21 22:00:18 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,4 @@
+export default function About() {
+  console.log("hello world");
+  return <div>hello about</div>;
+}
---
+++ pages/blog/index.js
@@ -0,0 +1,6 @@
+import Head from "next/head";
+
+export default function Blog() {
+  console.log("hello world");
+  return <div>hello blog</div>;
+}
